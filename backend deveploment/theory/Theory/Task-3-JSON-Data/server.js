8b9ac8fd20545588681a080e5a1f1f2d67f474fd
Kabir Choudhary <kaveer.chaudhary@gmail.com>
2026-08-21 14:54:30 +0530

    Add theory backend examples

---
+++ backend deveploment/theory/Theory/Task-3-JSON-Data/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+const students = [
+    { id: 1, name: "Aarav", branch: "CSE" },
+    { id: 2, name: "Diya", branch: "ECE" },
+    { id: 3, name: "Rohan", branch: "IT" }
+];
+
+app.get("/", (req, res) => {
+    res.send("Student Management API");
+});
+
+app.get("/students", (req, res) => {
+    res.json(students);
+});
+
+// Get students by branch
+app.get("/students/branch/:branch", (req, res) => {
+    const branch = req.params.branch.toLowerCase();
+
+    const filteredStudents = students.filter(
+        student => student.branch.toLowerCase() === branch
+    );
+
+    res.json(filteredStudents);
+});
+
+// Get student by ID
+app.get("/students/:id", (req, res) => {
+    const student = students.find(
+        s => s.id === parseInt(req.params.id)
+    );
+
+    if (student) {
+        res.json(student);
+    } else {
+        res.status(404).json({ error: "Student not found" });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server started at http://localhost:3000");
+});
